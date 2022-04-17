69e20348d5c7d6c559ba3388c4be9b0b71d7caa6
Harun-S <la200035@student.helha.be>
2022-04-17 12:35:41 +0200

    IT 4 : Developpement complet du fichier .c et .h de l'itération 4.

--- sources/marques.c
+++ sources/marques.c
@@ -2,20 +2,38 @@
 
 MYSQL *sqlConnection = NULL;
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]) 
 {
   setlocale(LC_ALL, "");
 
-  initialiserConnexion();
+  FILE *monFichier = fopen("../textfiles/marques_modeles.txt", "r");
 
-  remplirTables();
+  char *ligne = (char *)malloc(2000);
+  marque marque;
+  char *erreur = (char *)malloc(50);
+  marque.id = (char *)malloc(50);
+  marque.name = (char *)malloc(50);
+  marque.niceName = (char *)malloc(50);
+
+  initialiserConnexion();
+  executerSQL("DELETE FROM marque");
+
+  while (fscanf(monFichier, "%[^\n] ", ligne) != EOF)
+  {
+      recupFichier(ligne, marque);
+      recupMarque(marque, erreur);
+      remplirSQL(marque);
+  }
 
   cloturerConnexion();
+  fclose(monFichier);
+  free(erreur);
+  free(ligne);
 
   return EXIT_SUCCESS;
 }
 
-void initialiserConnexion()
+void initialiserConnexion() 
 {
   sqlConnection = mysql_init(NULL);
   if (sqlConnection == NULL)
@@ -32,46 +50,46 @@ void initialiserConnexion()
   }
 }
 
-void cloturerConnexion()
+void executerSQL(char *instructionSQL) 
+{
+  if (mysql_query(sqlConnection, instructionSQL))
+  {
+    fprintf(stderr, "%s\n", mysql_error(sqlConnection));
+    mysql_close(sqlConnection);
+    exit(EXIT_FAILURE);
+  }
+}
+
+void recupFichier(char *ligne, marque marque) 
+{
+  static char *virgule = ",";
+
+  strcpy(marque.id, strtok(ligne, virgule));
+  strcpy(marque.name, strtok(NULL, virgule));
+  strcpy(marque.niceName, strtok(NULL, virgule));
+}
+
+void recupMarque(marque marque, char *erreur) 
+{
+  jsonPrimitive(marque.id, "id", marque.id, 30, erreur);
+  jsonPrimitive(marque.name, "name", marque.name, 30, erreur);
+  jsonPrimitive(marque.niceName, "niceName", marque.niceName, 30, erreur);
+}
+
+void remplirSQL(marque marque) 
+{
+  int id;
+  id = atoi(marque.id);
+  char *insert = (char *)malloc(1024);
+
+  sprintf(insert, "INSERT INTO marque (id_marques, name_marques, niceName_marques) VALUES (%d, '%s', '%s')", id, marque.name, marque.niceName);
+  executerSQL(insert);
+}
+
+void cloturerConnexion() 
 {
   if (sqlConnection != NULL)
   {
     mysql_close(sqlConnection);
   }
-}
-
-void executerSQL(char *instructionSQL)
-{
-  if (mysql_query(sqlConnection, instructionSQL))
-  {
-    fprintf(stderr, "%s\n", mysql_error(sqlConnection));
-    mysql_close(sqlConnection);
-    exit(EXIT_FAILURE);
-  }
-}
-
-int insererDonnee(char *instructionSQL)
-{
-  if (mysql_query(sqlConnection, instructionSQL))
-  {
-    fprintf(stderr, "%s\n", mysql_error(sqlConnection));
-    mysql_close(sqlConnection);
-    exit(EXIT_FAILURE);
-  }
-  MYSQL_RES *sqlResult = mysql_store_result(sqlConnection);
-  if (sqlResult == NULL)
-  {
-    fprintf(stderr, "%s\n", mysql_error(sqlConnection));
-    mysql_close(sqlConnection);
-    exit(EXIT_FAILURE);
-  }
-  MYSQL_ROW sqlRow = mysql_fetch_row(sqlResult);
-  if (sqlRow == NULL)
-  {
-    fprintf(stderr, "%s\n", mysql_error(sqlConnection));
-    mysql_close(sqlConnection);
-    exit(EXIT_FAILURE);
-  }
-  int id = atoi(sqlRow[0]);
-  return id;
 }

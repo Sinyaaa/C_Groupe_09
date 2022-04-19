370655e51aff58d7e80836c22c4e37f78ae962b1
Sinyaaa <la200036@student.helha.be>
2022-04-19 03:59:10 +0200

    Ajout des fichiers versions.c & versions.h & testVersions.c

--- tests/testsJson.c
+++ tests/testsJson.c
@@ -3,10 +3,8 @@
 #include <locale.h>
 #include <time.h>
 
-<<<<<<< HEAD
-void test_taille_name()
+/* void test_taille_name()
 {
 
-}
-=======
->>>>>>> 0ff5eb823464bfa9e6897c1ef3072d96b4cbbb0c
+} */
+
